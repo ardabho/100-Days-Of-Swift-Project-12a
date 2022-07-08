4379c13b073a926be3b3f6e46ac1738060bec706
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-07-08 21:15:09 +0300

    Initial Commit

---
+++ 100 Days Of Swift-Project 10/Person.swift
@@ -0,0 +1,32 @@
+//
+//  Person.swift
+//  100 Days Of Swift-Project 10
+//
+//  Created by Arda Büyükhatipoğlu on 6.07.2022.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    
+    
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    
+}
